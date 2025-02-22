6ab0f50af7283e6347c67a7743cf51153dbeb675
Saidur Rahman <saidur@noice.id>
2025-02-23 04:08:19 +0600

    add tool call

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,7 @@
+import { createResource } from "@/lib/actions/resources";
 import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
+import { streamText, tool } from "ai";
+import { z } from "zod";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -9,7 +11,22 @@ export async function POST(req: Request) {
 
   const result = streamText({
     model: openai("gpt-4o"),
+    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "I could not find that information in my knowledge base."`,
     messages,
+    tools: {
+      addResource: tool({
+        description: `add a resource to your knowledge base.
+          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+        parameters: z.object({
+          content: z
+            .string()
+            .describe("the content or resource to add to the knowledge base"),
+        }),
+        execute: async ({ content }) => createResource({ content }),
+      }),
+    },
   });
 
   return result.toDataStreamResponse();
